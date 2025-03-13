afb49122d33754dd4d586a79d705b55130dd5a0b
Lia2406 <amaliakhoerina25@gmail.com>
2025-03-13 16:01:39 +0700

    Update mainquiz2ke2.c

--- mainquiz2ke2.c
+++ mainquiz2ke2.c
@@ -9,12 +9,6 @@ int main() {
     // 1) Masukkan bilangan 7 sebagai elemen pertama
     insertFirst(&list, 7);
 
-    // 3) Masukkan bilangan 9 di antara bilangan 7 dan 11
-    insertAfter(&list, 7, 9);
-
-    // 4) Masukkan bilangan 5 di awal list
-    insertFirst(&list, 5);
-
     // 5) Masukkan bilangan 13 di akhir list
     insertLast(&list, 13);
 
